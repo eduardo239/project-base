2d7f79b1e5a09f3392c0582727fdcd52919734c9
eduardo239 <eucrieiumaconta@gmail.com>
2025-10-25 14:12:03 -0300

    fix: comment out unused service account outputs in Terraform configuration

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,6 +1,6 @@
 
-output "service_account_key" {
-  description = "Service account key (sensitive)"
-  value       = var.create_key ? google_service_account_key.github_actions_key[0].private_key : null
-  sensitive   = true
-}
+# output "service_account_key" {
+#   description = "Service account key (sensitive)"
+#   value       = var.create_key ? google_service_account_key.github_actions_key[0].private_key : null
+#   sensitive   = true
+# }
