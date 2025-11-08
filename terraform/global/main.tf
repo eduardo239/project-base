d82f5b59b415a13818b97ba87ae7704b6240de29
eduardo239 <eucrieiumaconta@gmail.com>
2025-11-08 18:14:48 -0300

    feat: enhance Terraform workflow with retry logic for plan step and fix project name in main.tf

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -14,28 +14,7 @@ terraform {
 
 
 provider "google" {
-  project = "app-xyz-de"
+  project = "app-xyz-dev"
   region  = "us-central1"
 }
 
-# resource "random_id" "bucket_suffix" {
-#   byte_length = 4
-# }
-
-
-# resource "google_storage_bucket" "terraform_state" {
-#   name          = "terraform-state-bucket-${random_id.bucket_suffix.hex}"
-#   location      = "US"
-#   force_destroy = true
-
-#   uniform_bucket_level_access = true
-#   public_access_prevention    = "enforced"
-
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-
-#   versioning {
-#     enabled = true
-#   }
-# }
