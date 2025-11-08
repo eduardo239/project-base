32b461f8054bb1d9787dbcf2d8dc83abf148bfa1
eduardo239 <eucrieiumaconta@gmail.com>
2025-11-08 16:58:36 -0300

    feat: add cloud build module configuration for Terraform

---
+++ terraform/environments/dev/cloud-build.tf
@@ -0,0 +1,8 @@
+module "cloud-build" {
+  source = "../../modules/ci-cd/build"
+
+  github_owner = var.github_owner
+  github_repo  = var.github_repo
+  repo_name    = var.repo_name
+  project_id   = var.project_id
+}
