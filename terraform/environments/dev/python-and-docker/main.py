b38f6209c8cc03572c00296b95f12dbbdd078dc5
eduardo239 <eucrieiumaconta@gmail.com>
2025-11-08 18:39:11 -0300

    feat: add force-unlock option to Terraform validate step and implement new endpoint for retrieving items in Flask API

--- terraform/environments/dev/python-and-docker/main.py
+++ terraform/environments/dev/python-and-docker/main.py
@@ -40,6 +40,11 @@ def update_data():
         return jsonify({"status": "success", "message": "Data updated"}), 200
     return jsonify({"status": "error", "message": "No message provided"}), 400
 
+
+@app.route('/api/items', methods=['GET'])
+def get_items():
+    return jsonify(lista), 200
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
